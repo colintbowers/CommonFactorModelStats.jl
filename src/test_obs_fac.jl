1cfb6d300cfa72eaffe4d27329a131c1adb3dc0f
colintbowers <colintbowers@gmail.com>
2017-10-31 22:34:17 +1100

    working_on_obs_fac

--- src/test_obs_fac.jl
+++ src/test_obs_fac.jl
@@ -1,7 +1,8 @@
 
 struct ObsFactorTest
-
-
+    aj::Vector{Float64} #One element for each observed factor
+    mj::Vector{Float64} #One element for each observed factor
+    r2::Vector{Float64} #One element for each observed factor
 end
 
 """
@@ -30,6 +31,44 @@ function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::In
     ehat = Xcent - estfac*facload' #\tilde{e} from Bai, Ng (2006) [T*N]
     #Get eigenvalue inverse matrix
     invcapvtilde = diagm(1 ./ eigvalvec) #[numfac*numfac]
+    #Get gamma hat
+    if covmethod == :cshac || covmethod == :eq4 #Bai, Ng (2006) equation 4
+        nupper = Int(floor(min(sqrt(T), sqrt(N))))
+        #Method 1
+        capgammahat = zeros(Float64, numfac, numfac)
+        for t = 1:T
+            for i = 1:nupper
+                for j = 1:nupper
+                    capgammahat += facload[i, :] * facload[j, :]' * ehat[t, i] * ehat[t, j]
+                end
+            end
+        end
+        capgammahat = (1 / (nupper*T)) * capgammahat
+        #Method 2
+        capgammahat = zeros(Float64, numfac, numfac)
+        for i = 1:nupper
+            for j = 1:nupper
+                capgammahat += (1 / nupper) * facload[i, :] * facload[j, :]' * cgh_inner_sum(ehat, i, j)
+            end
+        end
+
+        cgh_inner_sum(ehat::Matrix{Float64}, i::Int, j::Int)::Float64 = (1 / size(ehat, 1)) * dot(view(ehat, :, i), view(ehat, :, j))
+
+
+    elseif covmethod == :eq5 #Bai, Ng (2006) equation 5
+        error("This variant is not currently available. Note for this equation, capgammahat depends on t, and so subsequent computations involving this estimator are more complicated and will need their own code path")
+        capgammahatvec = [ zeros(Float64, numfac, numfac) for t = 1:T ]
+        for t = 1:T
+            for i =  1:N
+                capgammahatvec[t] += ehat[t, i]^2 * facload[i, :] * facload[i, :]'
+            end
+            capgammahatvec[t] *= (1/N)
+        end
+    elseif covmethod == :eq6 #Bai, Ng (2006) equation 6
+        sigsqhat = (1/(T*N)) * sum(ehat.^2)
+        capgammaahat = (1/N) * sigsqhat * (facload' * facload) #[numfac*numfac]
+    end
+
     #Loop over observed factors
     for k = 1:size(obsfac, 2)
         #Get gammahat (least squares of estfac on observed factor)
@@ -38,19 +77,17 @@ function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::In
         capghat = estfac * gammahat
 
         #Get estimated variance of least squares estfac estimate of observed factor (capg)
-        varhatcapghat = (1/N) * gammahat' * invcapvtilde * capgammahat * invcapvtilde *  gammahat
+        if covmethod == :eq5 ; varhatcapghatvec = [ (1/N) * gammahat' * invcapvtilde * capgammahat * invcapvtilde *  gammahat for capgammahat in capgammahatvec ]
+        else ; varhatcapghat = (1/N) * gammahat' * invcapvtilde * capgammahat * invcapvtilde *  gammahat
+        end
+        #Get tau statistic
+        if covmethod == :eq5 ; tau = (capghat - capg) ./ sqrt.(varhatcapghatvec)
+        else ; tau = (capghat - capg) / sqrt(varhatcapghat)
+        end
+
+
     end
 
-    #Get gamma hat
-    if covmethod == :cshac || covmethod == :eq4 #Bai, Ng (2006) equation 4
-        error("Use chris code here as guide. Bai Ng paper bit confusing here as they seem to
-        use j as subscript for the true factors as well as for observed factors")
-    elseif covmethod == :eq5 #Bai, Ng (2006) equation 5
-
-    elseif covmethod == :eq6 #Bai, Ng (2006) equation 6
-        sigsqhat = (1/(T*N)) * sum(ehat.^2)
-        capgammaahat = (1/N) * sigsqhat * (facload' * facload) #[numfac*numfac]
-    end
 
 
 
