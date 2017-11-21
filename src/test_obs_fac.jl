74e92b52d4aeb5f2be2c42d3674acdf9fb7e02fe
colintbowers <colintbowers@gmail.com>
2017-11-21 22:23:11 +1100

    getting there

--- src/test_obs_fac.jl
+++ src/test_obs_fac.jl
@@ -1,4 +1,16 @@
 
+
+"""
+    ObsFactorTest
+
+Output type from the testobsfactor function. The fields of this type include: \n
+    -aj: Test statistic defined in Bai, Ng (2006) equation 2. aj <= alpha implies
+        the corresponding observed factor is close to spanning the latent factor
+        space. aj --> 1 implies the corresponding observed factor is unlikely
+        to span the latent factor space.
+    -mj:
+    -r2:
+"""
 struct ObsFactorTest
     aj::Vector{Float64} #One element for each observed factor
     mj::Vector{Float64} #One element for each observed factor
@@ -6,16 +18,19 @@ struct ObsFactorTest
 end
 
 """
-    testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number} ; kwargs)
-    testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::Int ; kwargs)
+    testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number} ; kwargs) \n
+    testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::Int ; kwargs) \n
 
 The function tests whether the observated factors in obsfac span the same space as the latent
 factor space that is estimated from input dataset x. The testing methodology is described in
-Bai, Ng (2006) Evaluating Latent and Observed Factors in Macroeconomics and Finance [Nournal of
-Econometrics].
+Bai, Ng (2006) Evaluating Latent and Observed Factors in Macroeconomics and Finance [Journal of
+Econometrics]. \n
+
+See ?ObsFactorTest for more information on the output type, and the meaning of the various
+statistics it contains. \n
 """
 function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::Int ;
-                       xcent::Bool=false, covmethod::Symbol=:cshac)::ObsFactorTest
+                       xcent::Bool=false, covmethod::Symbol=:cshac, alpha::Float64=0.05)::ObsFactorTest
     #Preliminaries
     (T, N) = (size(x, 1), size(x, 2))
     N < numfac && return ObsFactorTest()
@@ -32,6 +47,7 @@ function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::In
     #Get eigenvalue inverse matrix
     invcapvtilde = diagm(1 ./ eigvalvec) #[numfac*numfac]
     #Get gamma hat
+    error("You still need to test two methods and make sure they are identical")
     if covmethod == :cshac || covmethod == :eq4 #Bai, Ng (2006) equation 4
         nupper = Int(floor(min(sqrt(T), sqrt(N))))
         #Method 1
@@ -51,10 +67,6 @@ function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::In
                 capgammahat += (1 / nupper) * facload[i, :] * facload[j, :]' * cgh_inner_sum(ehat, i, j)
             end
         end
-
-        cgh_inner_sum(ehat::Matrix{Float64}, i::Int, j::Int)::Float64 = (1 / size(ehat, 1)) * dot(view(ehat, :, i), view(ehat, :, j))
-
-
     elseif covmethod == :eq5 #Bai, Ng (2006) equation 5
         error("This variant is not currently available. Note for this equation, capgammahat depends on t, and so subsequent computations involving this estimator are more complicated and will need their own code path")
         capgammahatvec = [ zeros(Float64, numfac, numfac) for t = 1:T ]
@@ -70,12 +82,14 @@ function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::In
     end
 
     #Loop over observed factors
+    mjvec = Vector{Float64}(size(obsfac, 2))
+    ajvec = Vector{Float64}(size(obsfac, 2))
+    r2vec = Vector{Float64}(size(obsfac, 2))
     for k = 1:size(obsfac, 2)
         #Get gammahat (least squares of estfac on observed factor)
         capg = obsfac[:, k]
         gammahat = estfac \ capg
         capghat = estfac * gammahat
-
         #Get estimated variance of least squares estfac estimate of observed factor (capg)
         if covmethod == :eq5 ; varhatcapghatvec = [ (1/N) * gammahat' * invcapvtilde * capgammahat * invcapvtilde *  gammahat for capgammahat in capgammahatvec ]
         else ; varhatcapghat = (1/N) * gammahat' * invcapvtilde * capgammahat * invcapvtilde *  gammahat
@@ -84,7 +98,12 @@ function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::In
         if covmethod == :eq5 ; tau = (capghat - capg) ./ sqrt.(varhatcapghatvec)
         else ; tau = (capghat - capg) / sqrt(varhatcapghat)
         end
-
+        abstau = abs.(tau)
+        #Get mj
+        mjvec[k] = maximum(abstau)
+        #Get aj
+        ajvec[k] = mean(abstau .> quantile(Normal(0,1), 1 - (alpha/2)))
+        #Get R2
 
     end
 
@@ -93,7 +112,7 @@ function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number}, numfac::In
 
 
 end
-
+cgh_inner_sum(ehat::Matrix{Float64}, i::Int, j::Int)::Float64 = (1 / size(ehat, 1)) * dot(view(ehat, :, i), view(ehat, :, j))
 
 function testobsfactor(x::Matrix{<:Number}, obsfac::Matrix{<:Number} ; kmax::Int=10,
                        xcent::Bool=false)::NumFactor
